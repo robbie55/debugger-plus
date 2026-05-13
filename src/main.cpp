5ff2d64fd85a2f694712fbee6e8ac9f118fc4495
Rob <robbie.mcl@icloud.com>
2026-05-13 11:42:03 -0500

    Add in main user input loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "debugplus/debug_session.h"
 #include "debugplus/lldb_context.h"
+#include "debugplus/parser/parser.h"
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -15,6 +16,13 @@ int main(int argc, char* argv[]) {
 
     DebugSession session{argv[1]};
     session.Launch();
+
+    parser::DebugCmd user_in{};
+    while (user_in != parser::kQuit) {
+      user_in = parser::GetInput();
+
+      // feed in user input to LLDB
+    }
 
     return 0;
   } catch (std::exception& e) {
